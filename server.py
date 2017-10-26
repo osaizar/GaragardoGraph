2b1e1ccd59eb8354365ec014541375f09644d08f
Oier Saizar <oierlas@gmail.com>
2017-10-26 09:24:26 +0200

    erroreak konpinduta

--- server.py
+++ server.py
@@ -6,7 +6,16 @@ ADDR = "0.0.0.0"
 
 app = Flask(__name__)
 
-@app.route("/"):
+# Importak
+@app.route("/assets/<path:path>")
+def get_asset(path):
+	return app.send_static_file("assets/"+path)
+
+@app.route("/images/<path:path>")
+def get_images(path):
+	return app.send_static_file("images"+path)
+
+@app.route("/")
 def get_index():
     return render_template("index.html")
 
